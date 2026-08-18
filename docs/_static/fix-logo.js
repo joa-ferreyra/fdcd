62a8e9913b5cae114cc65181293c9e898e2a5686
joaferreyra <jferreyra.fbioyf@gmail.com>
2026-08-18 19:15:29 -0300

    soluciono problema con logo

---
+++ docs/_static/fix-logo.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("img.logo.logo-img").forEach(function (img) {
+    img.src = "https://tuia-fceia-unr.github.io/fdcd/_static/fdcd_logo.png";
+  });
+});
